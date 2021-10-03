ee9f7a289aedc56d5e3db1105da49720b68fadfa
fistbumpfromdestiny <fredrik@wizardry.se>
2021-10-03 11:25:52 +0200

    Merged save_ and load_player.py, creates a player and playerfile.P if it doesn't already exists.

--- load_player.py
+++ load_player.py
@@ -1,7 +1,30 @@
 from world_creator import *
 import pickle
 
-with open(f'{player._name}.obj', 'rb') as f:
-    player = pickle.load(f)
+playername = input("What is your name? ")
 
-print(player.getLevel())
+def loginPlayer(playername):
+
+    try:
+        open(f'{playername.lower()}.P')
+        return True
+    except IOError:
+        return False
+
+def savePlayer(player):
+    with open(f'{player._name.lower()}.P', 'wb') as f:
+        pickle.dump(player, f)
+        print(f"Wrote {player._name.lower()}.P to file.")
+
+
+if loginPlayer(playername):
+    with open(f'{playername.lower()}.P', 'rb') as f:
+        player = pickle.load(f)
+
+else:
+    player = Character(f'{playername.title()}', [torch1])
+    savePlayer(player)
+
+print(player.getName()) 
+print(player.getStr())
+
