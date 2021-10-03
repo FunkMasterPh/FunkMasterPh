af64a7dc0a70821c442cef09ff49ff6005676eb8
fistbumpfromdestiny <fredrik@wizardry.se>
2021-10-03 16:19:20 +0200

    Added support for auto save function

--- account_handler.py
+++ account_handler.py
@@ -14,15 +14,21 @@ def loginPlayer(playername):
 def savePlayer(player):
     with open(f'{player._name.lower()}.P', 'wb') as f:
         pickle.dump(player, f)
-        print("Game saved.")
+        return True
+        
 
+def autoSave():
+    while True:
+        time.sleep(120)
+        savePlayer(player)
+        
 
 if loginPlayer(playername):
     with open(f'{playername.lower()}.P', 'rb') as f:
         player = pickle.load(f)
 
 else:
-    player = Character(f'{playername.title()}', [torch1])
+    player = Character(f'{playername.title()}', [axe2, torch1])
     savePlayer(player)
 
 print(player.getName()) 
