699533dbb4a5d258e040cc1afc3451175eee31d8
fistbumpfromdestiny <fredrik@wizardry.se>
2021-10-03 16:18:38 +0200

    Added auto save function

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from character_class import Character
 from room_class import *
 from goblin_class import *
 from items_class import *
-import time
+import threading
 import library
-import random
-import fight
-import commands
 import account_handler
 from world_creator import *
 
 currentRoom = cave_1
+saver = threading.Thread(target=account_handler.autoSave)
+saver.start()
 
-
-while True:
-    currentRoom = library.parsePlayerCommand(input("What do you want to do? > "), currentRoom)
+def main_menu(currentRoom):
+    while True:
+        currentRoom = library.parsePlayerCommand(input("What do you want to do? > "), currentRoom)
  
+if __name__ == '__main__':
+
+    main_menu(currentRoom)
+  
