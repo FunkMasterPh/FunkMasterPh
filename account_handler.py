3dfabbaf6c947a9e62ac1246dd8e7d8e9be79a44
fistbumpfromdestiny <fredrik@wizardry.se>
2021-10-04 18:03:12 +0200

    Saves player files in a unique folder

--- account_handler.py
+++ account_handler.py
@@ -1,13 +1,13 @@
 from world_creator import *
 from world_class import World
 import pickle
-import time
+import time, os
 
 playername = input("What is your name? ")
 def loginPlayer(playername):
     """"""
     try:
-        open(f'{playername.lower()}.P')
+        open(f'{playername}/{playername.lower()}.P')
         return True
     except IOError:
         return False
@@ -15,20 +15,20 @@ def loginPlayer(playername):
 def savePlayer(player):
 
     
-    with open(f'{player._name.lower()}.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}.P', 'wb') as f:
         pickle.dump(player, f)
 
 def saveWorld(cave_1, cave_2, cave_3, cave_4, cave_5):
     
-    with open(f'{player._name.lower()}_cave1.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}_cave1.P', 'wb') as f:
         pickle.dump(cave_1, f)
-    with open(f'{player._name.lower()}_cave2.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}_cave2.P', 'wb') as f:
         pickle.dump(cave_2, f)
-    with open(f'{player._name.lower()}_cave3.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}_cave3.P', 'wb') as f:
         pickle.dump(cave_3, f)
-    with open(f'{player._name.lower()}_cave4.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}_cave4.P', 'wb') as f:
         pickle.dump(cave_4, f)
-    with open(f'{player._name.lower()}_cave5.P', 'wb') as f:
+    with open(f'{playername}/{player._name.lower()}_cave5.P', 'wb') as f:
         pickle.dump(cave_5, f)
    
 def autoSave():
@@ -39,11 +39,13 @@ def autoSave():
         
 
 if loginPlayer(playername):
-    with open(f'{playername.lower()}.P', 'rb') as f:
+    with open(f'{playername}/{playername.lower()}.P', 'rb') as f:
         player = pickle.load(f)
         
 else:
     player = Character(f'{playername.title()}', [])
+
+    os.mkdir(f'{playername}')
     
 
 print(player.getName()) 
